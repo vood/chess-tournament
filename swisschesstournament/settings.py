a3f299891e837479d23242cb732ef5b0acf3b424
Artem Vysotsky <artem.vysotsky@gmail.com>
2014-02-22 22:43:33 -0800

    Heroku app

--- swisschesstournament/settings.py
+++ swisschesstournament/settings.py
@@ -36,8 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tournament',
-    'debug_toolbar'
+    'tournament'
 )
 
 MIDDLEWARE_CLASSES = (
